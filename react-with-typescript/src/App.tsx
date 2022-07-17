67466b6b0a6ce61821abfdced14e05448d4c31f1
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-17 10:45:17 +0100

    added interface & state to List component

---
+++ react-with-typescript/src/App.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+
+interface IState{
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+  }
+
+
+
+function App() {
+const [people, setPeople] = useState<IState["people"]>([]);
+
+  return (
+    <div className="App">
+     <h1>People invited to our Party </h1>
+     <List people={people} />
+    </div>
+  );
+}
+
+export default App;
