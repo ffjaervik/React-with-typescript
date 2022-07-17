7d58f6a10f94d5dc3cb58d9933ab06d4fe263ae4
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-17 14:51:58 +0100

    Completed task with handleclick event handler and passing states as props to AddToList component

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import List from './components/List';
 import AddToList from './components/AddToList';
 
-interface IState{
+export interface IState{
   people: {
     name: string,
     age: number,
@@ -15,12 +15,12 @@ interface IState{
 
 
 
-function App() {
+function App(){
 const [people, setPeople] = useState<IState["people"]>([
   {
     name: 'Max',
     age: 28,
-    url: '',
+    url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
     note: "Max is a bit of a jerk"
   },
 ]);
@@ -29,7 +29,7 @@ const [people, setPeople] = useState<IState["people"]>([
     <div className="App">
      <h1>People invited to our Party </h1>
      <List people={people} />
-      <AddToList/>
+      <AddToList people ={people} setPeople={setPeople}/>
     </div>
   );
 }
--- react-with-typescript/src/components/AddToList.tsx
+++ react-with-typescript/src/components/AddToList.tsx
@@ -1,31 +1,89 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { IState as Props } from "../App";
 
-const AddToList = () => {
+interface IProps {
+    setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+    people: Props["people"]
+}
+
+const AddToList: React.FC<IProps> = ({setPeople, people}) => {
+
     const [input, setInput] = useState({
         name: "",
         age: "",
         note: "",
-        img: ""
-    })
+        url: ""
+    }) 
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setInput({
             ...input,
             [e.target.name]: e.target.value
         })
     }
 
+    const handleClick = () => {
+        if(!input.name || !input.age) return
+
+        setPeople([
+            ...people,
+            {
+                name: input.name,
+                age: parseInt(input.age),
+                url: input.url,
+                note: input.note
+            }
+        ]);
+
+        setInput({
+            name: "",
+            age: "",
+            url: "",
+            note: ""
+        })
+    }
 
     return (
         <div className="AddToList">
-            <input type="text" placeholder="Name" className="AddToList-input" value ={input.name} onChange = {handleChange} name="name" />
-            <input type="text" placeholder="Age" className="AddToList-input" value ={input.age} onChange = {handleChange} name="age"/>
-            <input type="text" placeholder="Image URL" className="AddToList-input" value ={input.img} onChange = {handleChange} name= "img"/>
-            <textarea placeholder="Note" className="AddToList-input" value ={input.note} onChange = {handleChange} name ="note"/>
-
-   
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="name"
+                value={input.name}
+                placeholder="Name"
+            />
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="age"
+                value={input.age}
+                placeholder="Age"
+            />
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="url"
+                value={input.url}
+                placeholder="Image Url"
+            />
+            <textarea
+                onChange={handleChange}
+                className="AddToList-input"
+                name="note"
+                value={input.note}
+                placeholder="Note"
+            />
+            <button
+                onClick={handleClick}
+                className="AddToList-btn"
+            >
+                Add to List
+            </button>
         </div>
     )
 }
 
-export default AddToList;
+export default AddToList
